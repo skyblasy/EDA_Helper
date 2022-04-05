86124af42f88fb92d3ab6890171e8be405bcebaf
Schuyler Blasy <schuylerblasy@Schuylers-MacBook-Pro.local>
2022-04-05 14:51:10 -0700

    new function
    created a new function that creates a correlation matrix and heatmap of variables

--- EDA_Helper.py
+++ EDA_Helper.py
@@ -22,3 +22,25 @@ class EDA_Check:
         
     def valuechart(self,value):
         sns.heatmap(df.eq(value),yticklabels=False,cbar=False,cmap="mako")
+    
+    def corrmap(self):
+        corr_matrix = df.corr()
+        mask = np.zeros_like(corr_matrix, dtype=np.bool)
+        mask[np.triu_indices_from(mask)]= True
+        
+        f, ax = plt.subplots(figsize=(20, 25)) 
+        heatmap = sns.heatmap(corr_matrix, 
+                              mask = mask,
+                              square = True,
+                              linewidths = .5,
+                              cmap = 'coolwarm',
+                              cbar_kws = {'shrink': .4, 
+                                        'ticks' : [-1, -.5, 0, 0.5, 1]},
+                              vmin = -1, 
+                              vmax = 1,
+                              annot = True,
+                              annot_kws = {'size': 12})
+        #add the column names as labels
+        ax.set_yticklabels(corr_matrix.columns, rotation = 0)
+        ax.set_xticklabels(corr_matrix.columns)
+        sns.set_style({'xtick.bottom': True}, {'ytick.left': True})
